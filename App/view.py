77039fc6fba403855eb7253a853e1434cc44a9bd
JuilanaDC15 <j.delgadillo@uniandes.edu.co>
2021-08-12 09:48:18 -0500

    solución TODO1

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Modificaciones para el laboratorio 1.
+    print("3- Cargar Opcion 3")
     print("0- Salir")
 
 
